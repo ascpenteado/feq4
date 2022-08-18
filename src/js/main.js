b48d0afd12d1dc102b000bdc6d37b304cc12c31c
Arthur Penteado <ascpenteado@gmail.com>
2022-08-17 23:29:24 -0300

    initial configs and fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,35 @@
-var reportsWidget = {
-    options: {
-        containerSelector: '.reports',
-        template: (
-            '{{#.}}' +
-                '<article class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
-                    '<footer class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
-        )
-    },
+const template = `
+{{#.}}
+    <article class="reports_item">
+        <a href="{{cover}}" target="_blank">
+            <img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>
+        </a>
+        <footer class="reports_docs">
+            {{#documents}}
+                <h3 class="reports_title">
+                    <a href="{{url}}" target="_blank">{{title}} <span>({{file_size}} {{file_type}})</span></a>
+                </h3>
+            {{/documents}}
+        </footer>
+    </article>
+{{/.}}
+`;
 
-    init: function() {
-        this.renderReports(reportData || []);
-    },
+const reportsWidget = {
+  options: {
+    containerSelector: ".reports",
+    template,
+  },
 
-    renderReports: function(reports) {
-        var inst = this,
-            options = inst.options;
+  renderReports: function (reports) {
+    const { containerSelector, template } = this.options;
 
-        $(options.containerSelector).html(Mustache.render(options.template, reports));
-    }
+    $(containerSelector).html(Mustache.render(template, reports));
+  },
+
+  init: function () {
+    this.renderReports(reportData || []);
+  },
 };
 
 reportsWidget.init();
